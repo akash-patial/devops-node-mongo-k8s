5d5aa31aa324ae50a7c11a1c34256bc20ec856f3
Akash Patial <akash.patial@pertsol.com>
2026-02-18 21:40:32 +0530

    tf files

--- tf/main.tf
+++ tf/main.tf
@@ -16,11 +16,66 @@ provider "aws" {
 
 resource "aws_instance" "ec2server" {
 
-    ami = "ami-04c54313c5ae6bbcb"
+    ami = "ami-0ffef61f6dc37ae89"
     instance_type = "t3.micro"
+    key_name = aws_key_pair.ec2key.key_name
+    security_groups = [aws_security_group.my-sg.name]
+  
+
+    root_block_device {
+      volume_size = 15
+      volume_type = "gp3"
+    }
 
     tags ={
         Name="first_ec2"
     }
   
 }
+
+resource "aws_key_pair" "ec2key" {
+      key_name = "tf-ec2key"
+      public_key = file("tf-ec2-key.pub")
+
+}
+
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
+
+resource "aws_security_group" "my-sg" {
+  name        = "automate-sg"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+
+    to_port = 22
+    from_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "SSH open"
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol= "tcp"
+    description = "http open"
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol = "-1"
+    description = "Allow access opened"
+  }
+
+  tags = {
+    Name = "automate-sg"
+  }
+}
+
